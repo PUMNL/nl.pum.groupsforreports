f8bd6409891688b7d32b68ead88cea796add3cf6
Erik Hommel <hommel@ee-atwork.nl>
2016-04-08 16:42:14 +0200

    fix issue 3245

--- CRM/Groupsforreports/GroupReport.php
+++ CRM/Groupsforreports/GroupReport.php
@@ -27,15 +27,37 @@ class CRM_Groupsforreports_GroupReport
     );
     $this->_reports['CRM_Civireports_Form_Report_MyProjectIntake'] = $groups;
     $this->_reports ['CRM_Casereports_Form_Report_PumProjects'] = $groups;
+    $this->_reports ['CRM_Threepeas_Form_Report_PumProjects'] = $groups;
     $groups[] = 'Grant_Coordinators_96';
     $this->_reports['CRM_Casereports_Form_Report_MainActivities'] = $groups;
     foreach ($groups as $groupName) {
       try {
-        $groupId = civicrm_api3('Group', 'Getvalue', array('name' => $groupName, 'return' => 'id'));
-        $this->_groups[$groupName] = $groupId;
+        $apiGroupId = civicrm_api3('Group', 'Getvalue', array('name' => $groupName, 'return' => 'id'));
+        $groupId = (int) $apiGroupId;
+        $this->_groups[$groupId] = $groupName;
       } catch (CiviCRM_API3_Exception $ex) {}
     }
   }
+
+  /**
+   * Method to get group members
+   *
+   * @param int $groupId
+   * @return array
+   */
+  private function getGroupMembers($groupId) {
+    $result = array();
+    $groupContactParams = array('group_id' => $groupId, 'options' => array('limit' => 99999));
+    try {
+      $members = civicrm_api3('GroupContact', 'Get', $groupContactParams);
+      foreach ($members['values'] as $member) {
+        $result[$member['contact_id']] = $member['contact_id'];
+      }
+    } catch (CiviCRM_API3_Exception $ex) {
+    }
+    return $result;
+  }
+
 
   /**
    * Method to get the group ids for the report
@@ -44,12 +66,12 @@ class CRM_Groupsforreports_GroupReport
    * @return array
    * @static
    */
-  public static function getGroupsForReport($reportClass) 
+  public static function getGroupMembersForReport($reportClass) 
   {
     $result = array();
     $groupsForReports = new CRM_Groupsforreports_GroupReport();
-    foreach ($groupsForReports->_reports[$reportClass] as $groupName) {
-      $result[] = $groupsForReports->_groups[$groupName];
+    foreach ($groupsForReports->_reports[$reportClass] as $groupId) {
+      $result = $result + $groupsForReports->getGroupMembers($groupId);
     }
     return $result;
   }
